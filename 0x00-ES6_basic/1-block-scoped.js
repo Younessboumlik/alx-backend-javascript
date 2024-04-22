9c8f71f613f11429a840ddc3e923c0ff95287f0d
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-04-22 20:00:40 +0100

    task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,11 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    (function() {
+      let task = true;
+      let task2 = false;
+      // You can use task and task2 here.
+    })();
   }
 
   return [task, task2];
