eee0b60eb903fa82f81b02882b8bded4ef759223
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-05-01 03:41:34 +0100

    task-

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export function getListStudents(){
+let std1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco'};
+let std2 =  { id: 2, firstName: 'James', location: 'Columbia' };
+let std3 =  { id: 5, firstName: 'Serena', location: 'San Francisco' };
+let array = [std1,std2,std3];
+return array;
+}
