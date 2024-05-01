137bcc7c8a4527879d2a9d8b9f57cd7713eac5c0
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-05-01 03:43:07 +0100

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export function getListStudents(){
-let std1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco'};
-let std2 =  { id: 2, firstName: 'James', location: 'Columbia' };
-let std3 =  { id: 5, firstName: 'Serena', location: 'San Francisco' };
-let array = [std1,std2,std3];
-return array;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
