8c964641cbf9e1112fcc79785180e9511579e31a
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-05-01 03:54:51 +0100

    task2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-export function default getStudentsByLocation(stds,city){
+export default function getStudentsByLocation(stds,city){
     if (stds instanceof Array) {
     return stds.filter((student) => student.location === city);
   }
