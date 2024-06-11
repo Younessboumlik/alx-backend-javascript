35c4ac01579be0d684a8f282295e1efb71464bcd
root <root@Ubuntu.myguest.virtualbox.org>
2024-06-11 05:12:08 +0100

    classes in js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom.js'
 
 export default function initializeRooms(){
-	let class1 = ClassRoom(19);
-	let class2 = ClassRoom(20);
-	let class3 = ClassRoom(34);
+	let class1 = new ClassRoom(19);
+	let class2 = new ClassRoom(20);
+	let class3 = new ClassRoom(34);
 	let  classrooms = [];
 	classrooms.push(class1);
 	classrooms.push(class2);
