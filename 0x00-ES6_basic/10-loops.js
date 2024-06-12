85c13bffb94a8cb9ed6715c2ee6216ad4941ca16
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-12 05:34:51 +0100

    task10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const elem of array) {
-    var indx = array.indexOf(elem);
+    const indx = array.indexOf(elem);
     array[indx] = appendString + elem;
   }
 
