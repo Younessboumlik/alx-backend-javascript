445c911e7831a6d6c1b10b871c28e19f16ab787f
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-14 01:18:24 +0100

    fixing style

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,4 @@
 export default class HolbertonCourse {
-
   constructor(name, length, students) {
     this.name = name;
     this.length = length;
@@ -8,25 +7,31 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+  
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    
     this._name = value;
   }
+  
   get length(){
     return this._length;
   }
+  
      set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
+       
     this._length = value;
   }
 
   get students() {
     return this._students;
   }
+  
   set students(value) {
     if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
@@ -36,5 +41,4 @@ export default class HolbertonCourse {
     }
     this._students = value;
   }
-
 }
