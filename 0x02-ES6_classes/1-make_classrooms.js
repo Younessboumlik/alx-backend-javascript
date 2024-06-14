685bc5fb9a9562e45b9e5002243e5dfb7151be9a
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-14 01:16:40 +0100

    fixing style

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,14 +4,13 @@ import ClassRoom from './0-classroom.js'
  * @return array of classrooms.
  */
 export default function initializeRooms(){
-	let class1 = new ClassRoom(19);
-	let class2 = new ClassRoom(20);
-	let class3 = new ClassRoom(34);
-	let  classrooms = [];
-	classrooms.push(class1);
-	classrooms.push(class2);
-	classrooms.push(class3);
+  let class1 = new ClassRoom(19);
+  let class2 = new ClassRoom(20);
+  let class3 = new ClassRoom(34);
+  let  classrooms = [];
+  classrooms.push(class1);
+  classrooms.push(class2);
+  classrooms.push(class3);
 
-	return classrooms;
-
+  return classrooms;
 }
