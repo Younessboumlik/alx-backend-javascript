d5c088212c5ab92ec20bbf77decba3b3e400a7e2
root <root@Ubuntu.myguest.virtualbox.org>
2024-06-19 03:25:53 +0100

    faxing task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 /**
  * Taking input and then output it.
  */
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (inp) =>
 {
